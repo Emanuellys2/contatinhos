3609be57a7d70b90d40a4ea4b35efd2bdcf46789
Emanuellys2 <duarte.emanuellys2@gmail.com>
2024-09-05 16:58:59 -0300

    :sparkles: feat: Configuração StatusBar com estilo light-content

--- src/app/home/styles.ts
+++ src/app/home/styles.ts
@@ -1,7 +1,13 @@
+import { theme } from "@/theme"
 import { StyleSheet } from "react-native"
 
 export const styles = StyleSheet.create({
     container: {
         flex: 1,
-    } 
+    },
+    header: {
+        width: "100%",
+        height: 132,
+        backgroundColor: theme.colors.blue,
+    },
 })
