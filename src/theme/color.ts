210ac34096b28010985301c757e7eac795f34f19
Emanuellys2 <duarte.emanuellys2@gmail.com>
2024-09-05 16:45:25 -0300

    :sparkles: feat: implementação do componente Loading

---
+++ src/theme/color.ts
@@ -0,0 +1,10 @@
+import { styles } from "@/app/home/styles"
+import { ActivityIndicator } from "react-native"
+
+export const colors = {
+    white: "#FFFFFF",
+    black: "#000000",
+
+    blue: "#4442F0",
+
+}
